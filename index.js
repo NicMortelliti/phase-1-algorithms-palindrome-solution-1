f17ac407029e10d8725b484891401dda102013f4
NicMortelliti <npmortelliti@gmail.com>
2022-02-28 13:20:39 -0800

    commit tree fdc67b9034a867d8a55ca75db0c19514c3cb4c41

--- index.js
+++ index.js
@@ -1,25 +1,44 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+  const reversedWord = word.split("").reverse().join("");
+
+  return reversedWord === word;
 }
 
 /* 
-  Add your pseudocode here
+  Add your pseudocode here:
+  
+  const reversedWord = string.split("").reverse().join("");
+
+  return reversedWord === word // true (or false)
 */
 
 /*
-  Add written explanation of your solution here
+  Add written explanation of your solution here:
+
+  The algorithm will first create a variable to contain the reversed string.
+  Then it will split the passed in string using split(),
+  then it will reverse the split string using reverse(),
+  then it will join the split/reversed string back together,
+  then it will store this result in the new variable we created at the top of the function.
+  
+  The function will then have a return statement wherein the new string will be compared to the passed in string, returning true or false.
+  
 */
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
   // add your own custom tests in here
   console.log("Expecting: true");
-  console.log("=>", isPalindrome("racecar"));
+  console.log("racecar =>", isPalindrome("racecar"));
+  console.log("wow =>", isPalindrome("wow"));
+  console.log("saippuakivikauppias =>", isPalindrome("saippuakivikauppias"));
 
   console.log("");
 
   console.log("Expecting: false");
-  console.log("=>", isPalindrome("robot"));
+  console.log("robot =>", isPalindrome("robot"));
+  console.log("javascript =>", isPalindrome("javascript"));
+  console.log("encyclopedia =>", isPalindrome("encyclopedia"));
 }
 
 module.exports = isPalindrome;
